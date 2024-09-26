c4bbc1d2d8ce7d70576816c49861a31004b6237a
Eric Mendes Moreira <eric.moreira@alunoescolassociais.org.br>
2024-09-26 10:15:37 -0300

    Muda a fonte de CSS para Bootstrap5, Adiiciona um escutador ao JS e a função de consumo do ViaCEP

--- cadastro.js
+++ cadastro.js
@@ -2,22 +2,19 @@
 //esse modo faz com que o java script opere de forma mais segura e rigorosa, ajudando a evitar erros comums de programação /* consumo de api - https://viacep.com.br/*/
  
 //função para limpar campos preenchidos
-const limparformularios = () => {
-    document.getElementById('logradouro').value = endereco.logradouro
-    = '';
-    document.getElementById('bairro').value = endereco.bairro
-    = '';
-    document.getElementById('localidade').value = endereco.localidade
-    = '';
-    document.getElementById('uf').value =endereco.uf
-    = '';
+const limparformulario = () => {
+    document.getElementById('logradouro').value = endereco.logradouro = '';
+    document.getElementById('bairro').value = endereco.bairro = '';
+    document.getElementById('localidade').value = endereco.localidade = '';
+    document.getElementById('uf').value = endereco.uf = '';
+  
 }
 //verifica se o cep é valido
 const enumero = (numero)=> /^[0-9]+$/.test(numero);
-const cepvalido = (cep) => cep.length == 8 && enumero(cep)
+const cepvalido = (cep) => cep.length == 8 && eNumero(cep)
  
  
-const preencherformulario = (endereço) => {
+const preencherformulario = (endereco) => {
     document.getElementById('logradouro').value = endereco.logradouro;
     document.getElementById('bairro').value = endereco.bairro;
     document.getElementById('localidade').value = endereco.localidade;
@@ -26,5 +23,28 @@ const preencherformulario = (endereço) => {
 
 // Função para consumo API ViaCEP
 const pesquisarCep = async() => {
-    
+    limparformulario();
+    const url = `http://viacep.com.br/ws/${cep.value}/json/`;
+
+    if(cepvalido(cep.value)){
+        const dados = await fetch(url);
+        const addres = await dados.json();
+
+        if(addres.hasOwnProprety('erro')){
+        // hasOwnProperty define verdadeiro ou falso se ele tem uma propriedade no Objeto
+            alert('CEP não encontrado');
+        }else{
+            preencherformulario(adress);
+        }
+        
+         
+    }else{alert (`CEP incorreto`)
+
+    }
+
 }
+
+// Chama o escutador para disparar ação de preenchimento
+document.getElementById('CEP').addEventListener('focusout',pesquisarCep);    
+// fetch faz uma requisição da url, e o guarda como dados
+        // await uma requisição de espera
